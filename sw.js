aa73a2ad7877c92bb4b4341a71ef5bd69f277d44
AKARuberDuck <ss2875356@gmail.com>
2025-07-02 16:56:11 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-// sw.js – Caches your system for offline deployment
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("lockdown-v1").then(cache => {
-      return cache.addAll([
-        "./",
+const cacheName = "terminal-lockdown-v1.0.0";
+const assetsToCache = [
+  "./",
         "./index.html",
         "./manifest.json",
         "./icon-192.png",
@@ -28,13 +24,16 @@ self.addEventListener("install", event => {
         "./renderer/glitch.js",
         "./renderer/mutator.js",
         "./renderer/uiEnhancements.js"
-      ]);
-    })
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assetsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
