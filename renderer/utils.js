041e2e67bbf4c679060d07a24d5430c44c392c6d
AKARuberDuck <ss2875356@gmail.com>
2025-07-03 14:39:08 -0500

    Update utils.js

--- renderer/utils.js
+++ renderer/utils.js
@@ -3,14 +3,11 @@ function getRandom(arr) {
 }
 
 function wordToAscii(word, format) {
-  return word
-    .split("")
-    .map(ch => {
-      const code = ch.charCodeAt(0);
-      if (format === "[DEC]") return code.toString(10);
-      if (format === "[HX]") return code.toString(16).toUpperCase();
-      if (format === "[OCT]") return code.toString(8);
-      return code;
-    })
-    .join(" ");
+  return word.split("").map(char => {
+    const code = char.charCodeAt(0);
+    if (format === "[DEC]") return code.toString(10);
+    if (format === "[HX]") return code.toString(16).toUpperCase();
+    if (format === "[OCT]") return code.toString(8);
+    return code;
+  }).join(" ");
 }
