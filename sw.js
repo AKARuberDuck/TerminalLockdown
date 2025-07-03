28124667fb86069f37bd4fd42c7f69fc27cde10d
AKARuberDuck <ss2875356@gmail.com>
2025-07-03 14:47:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,27 @@
-const cacheName = "terminal-lockdown-v1.0.0";
-const assetsToCache = [
+const cacheName = "lockdown-cache-v1.0.0";
+const assets = [
   "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png",
-        "./renderer/style.css",
-        "./renderer/app.js",
-        "./renderer/utils.js",
-        "./renderer/riddles.js",
-        "./renderer/profiles.js",
-        "./renderer/badges.js",
-        "./renderer/quantum.js",
-        "./renderer/modifiers.js",
-        "./renderer/fontRenderer.js",
-        "./renderer/banner.js",
-        "./renderer/generator.js",
-        "./renderer/riddleAI.js",
-        "./renderer/sandbox.js",
-        "./renderer/codex.js",
-        "./renderer/journal.js",
-        "./renderer/metrics.js",
-        "./renderer/glitch.js",
-        "./renderer/mutator.js",
-        "./renderer/uiEnhancements.js"
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "renderer/style.css",
+  "renderer/app.js",
+  "renderer/utils.js",
+  "renderer/riddles.js",
+  "renderer/generator.js",
+  "renderer/riddleAI.js",
+  "renderer/quantum.js",
+  "renderer/sandbox.js",
+  "renderer/codex.js",
+  "renderer/metrics.js",
+  "renderer/journal.js",
+  "renderer/badges.js",
+  "renderer/uiEnhancements.js"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(assetsToCache))
-  );
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
 self.addEventListener("fetch", (e) => {
